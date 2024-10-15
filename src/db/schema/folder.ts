d70cadf708de13f33abf5c2354f2f829022b430a
Muchiri <developedbyrobbie@gmail.com>
2024-10-15 15:02:48 +0300

    feat

--- src/db/schema/folder.ts
+++ src/db/schema/folder.ts
@@ -1,14 +1,20 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { AnyPgColumn, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm"
 import { FileTable } from './file';
 
 export const FolderTable = pgTable("post", {
     folderId: uuid("folderId").defaultRandom().primaryKey(),
+    parentFolderId: uuid("parentFolderId").references((): AnyPgColumn => FolderTable.folderId, { onDelete: "cascade" }),
     name: varchar("name").notNull()
 });
 
 export const FolderTableRelations = relations(FolderTable,
-    ({ many }) => ({
+    ({ one, many }) => ({
+        parent: one(FolderTable, {
+            fields: [FolderTable.folderId],
+            references: [FolderTable.folderId],
+        }),
+        children: many(FolderTable),
         files: many(FileTable)
     })
 );
